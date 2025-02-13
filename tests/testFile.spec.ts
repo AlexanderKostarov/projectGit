52bfd30db776414a9096d049d09dd19dd56aeadd
Alexander_Kost <alexander.kostarov@gmail.com>
2025-02-13 14:40:52 +0400

    deleted comment 1

--- tests/testFile.spec.ts
+++ tests/testFile.spec.ts
@@ -3,7 +3,7 @@ import { test } from "@playwright/test";
 test("Step 1 - login to mail", async ({ page }) => {
     await page.goto("https://mailfence.com/");
     await page.locator("#signin").click();
-    //await page.getByText("Log in").click();
+
     await page.locator("#UserID").fill("alik");
     await page.locator("#Password").fill("sEepqTaxB8P5@qw");
     await page.locator('[value="Enter"]').click();
